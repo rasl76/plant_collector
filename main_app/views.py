a0010f2820680c60628a507947ee9a55a7e60eeb
Renee Storey-Lussier <reneesl@Renees-MBP.royaume.com>
2022-11-08 17:37:41 -0500

    cbv lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Plant
 
 # Add the following import
@@ -22,7 +23,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>:)</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
@@ -35,3 +36,10 @@ def plants_index(request):
 def plants_detail(request, plant_id):
   plant = Plant.objects.get(id=plant_id)
   return render(request, 'plants/detail.html', { 'plant': plant })
+
+# View Class
+
+class PlantCreate(CreateView):
+  model = Plant
+  fields = '__all__'
+  success_url = '/cats/'
